40995f161013dc446d97d65772046388e091c023
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-10-11 16:00:40 +0300

    use v-if and v-else

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>{{ header }}</h1>
-  <form @submit.prevent="saveItem" class="add-item-form">
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" @click="doEdit(false)" class="btn">Cancel</button>
+    <button v-else @click="doEdit(true)" class="btn btn-primary">Add item</button>
+  </div>
+  <form v-if="editing" @submit.prevent="saveItem" class="add-item-form">
     <input v-model.trim="newItem" type="text" placeholder="Add an item" />
     <label>
       <input type="checkbox" v-model="newInputHighPriority" />
@@ -11,12 +15,14 @@
   <ul>
     <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
   </ul>
+  <p v-if="!items.length">Nothing to see here</p>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const header = ref('Shopping List App');
+const editing = ref(false);
 const items = ref([
   { id: 1, label: '10 party hats' },
   { id: 2, label: '2 board games' },
@@ -27,6 +33,10 @@ const newInputHighPriority = ref(false);
 
 const saveItem = () => {
   items.value.push({ id: items.value.length + 1, label: newItem.value });
+  newItem.value = '';
+};
+const doEdit = (e) => {
+  editing.value = e;
   newItem.value = '';
 };
 </script>
