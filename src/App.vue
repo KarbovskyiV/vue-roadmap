406d83e71d1990a339715b8825b50e08c3a2647a
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-10-11 16:42:21 +0300

    use dynamic class

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,20 @@
   <form v-if="editing" @submit.prevent="saveItem" class="add-item-form">
     <input v-model.trim="newItem" type="text" placeholder="Add an item" />
     <label>
-      <input type="checkbox" v-model="newInputHighPriority" />
+      <input type="checkbox" v-model="newItemHighPriority" />
       High priority
     </label>
     <button :disabled="newItem.length === 0" class="btn btn-primary">Save Item</button>
   </form>
   <ul>
-    <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
+    <li
+      v-for="item in items"
+      :key="item.id"
+      @click="togglePurchased(item)"
+      :class="{ strikeout: item.purchased, priority: item.highPriority }"
+    >
+      {{ item.label }}
+    </li>
   </ul>
   <p v-if="!items.length">Nothing to see here</p>
 </template>
@@ -24,19 +31,26 @@ import { ref } from 'vue';
 const header = ref('Shopping List App');
 const editing = ref(false);
 const items = ref([
-  { id: 1, label: '10 party hats' },
-  { id: 2, label: '2 board games' },
-  { id: 3, label: '20 cups' },
+  { id: 1, label: '10 party hats', purchased: true, highPriority: false },
+  { id: 2, label: '2 board games', purchased: true, highPriority: false },
+  { id: 3, label: '20 cups', purchased: false, highPriority: true },
 ]);
 const newItem = ref('');
-const newInputHighPriority = ref(false);
+const newItemHighPriority = ref(false);
 
 const saveItem = () => {
-  items.value.push({ id: items.value.length + 1, label: newItem.value });
+  items.value.push({ id: items.value.length + 1, label: newItem.value, highPriority: newItemHighPriority.value });
   newItem.value = '';
+  newItemHighPriority.value = false;
 };
+
 const doEdit = (e) => {
   editing.value = e;
   newItem.value = '';
+  newItemHighPriority.value = false;
+};
+
+const togglePurchased = (item) => {
+  item.purchased = !item.purchased;
 };
 </script>
