4379545533cd6c416242ff034dc957b6a631bb0c
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-10-11 13:20:50 +0300

    use v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="{ id, label } in items" :key="id"> {{ label }}</li>
+  </ul>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const header = ref('Shopping List App');
+const items = ref({
+  1: { id: 1, label: '10 party hats' },
+  2: { id: 2, label: '2 board games' },
+  3: { id: 3, label: '20 cups' },
+});
 </script>
