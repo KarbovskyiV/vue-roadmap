6c3b8b64e299c6ff22c926bca1791700600d796a
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-10-11 13:11:01 +0300

    add main styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg" />
+  <h1>{{ header }}</h1>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const msg = ref('Hello World!');
+const header = ref('Shopping List App');
 </script>
